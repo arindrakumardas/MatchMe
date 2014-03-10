733c356da69f4ce8f08e85c72c02195a005d0485
irene <paspatoufi@gmail.com>
2014-03-10 14:04:10 +0100

    implemented soundBtn & added id's to layouts

--- src/se/kth/csc/iprog/matchme/android/StartActivity.java
+++ src/se/kth/csc/iprog/matchme/android/StartActivity.java
@@ -1,19 +1,29 @@
 package se.kth.csc.iprog.matchme.android;
 
 import se.kth.csc.iprog.matchme.android.view.StartView;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 
 public class StartActivity extends Activity {
+	
+	private AudioManager mAudioManager;
+	private boolean mPhoneIsSilent;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // Default call to load previous state
     	super.onCreate(savedInstanceState);
+    	
+    	// get info for audio manager
+    	mAudioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
+    	checkIfPhoneIsSilent();
     	
     	// Set the view for the main activity screen
     	// it must come before any call to findViewById method
@@ -33,15 +43,61 @@ public class StartActivity extends Activity {
 	    		//startActivity(i);
 			}
     	});
+    	
     	Button highScoreBtn = (Button) findViewById(R.id.view_highscore);
     	highScoreBtn.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
 				//TODO: Assign this to move to the next view!
-				//Intent i = new Intent(MainActivity.this, MenuActivity.class);
-	    		//startActivity(i);
+//				Intent i = new Intent(MainActivity.this, LevelActivity.class);
+//	    		startActivity(i);
 			}
     	});
+    	
+    	Button toggleSoundBtn = (Button) findViewById(R.id.volume);
+    	toggleSoundBtn.setOnClickListener(new OnClickListener(){
+    		@Override
+    		public void onClick(View v) {
+    			if (mPhoneIsSilent) {
+    				mAudioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+    				mPhoneIsSilent = false;
+    			} else {
+    				mAudioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+    				mPhoneIsSilent = true;
+    			}
+    			toggleUI();
+    		}
+    	});
     }
+
+    @Override
+    protected void onResume() {
+    	super.onResume();
+    	checkIfPhoneIsSilent();
+    	toggleUI();
+    }
+    
+	private void checkIfPhoneIsSilent() {
+		int ringerMode = mAudioManager.getRingerMode();
+		if (ringerMode == AudioManager.RINGER_MODE_SILENT) {
+			mPhoneIsSilent = true;
+		} else {
+			mPhoneIsSilent = false;
+		}
+	}
+	
+	// Toggles the UI images from silent to normal and vice-versa
+	private void toggleUI() {
+		ImageView imageView = (ImageView) findViewById(R.id.volume);
+		Drawable soundIcon;
+		
+		if (mPhoneIsSilent) {
+			soundIcon = getResources().getDrawable(R.drawable.volume_off);
+		} else {
+			soundIcon = getResources().getDrawable(R.drawable.volume_on);
+		}
+		imageView.setImageDrawable(soundIcon);
+		
+	}
 
 }
