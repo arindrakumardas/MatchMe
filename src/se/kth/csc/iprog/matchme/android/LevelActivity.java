bf1321dbae002b0a18282b079bbd9d753c33c54b
pakaki <tziova@kth.se>
2014-03-12 13:31:15 +0100

    level 1 button

--- src/se/kth/csc/iprog/matchme/android/LevelActivity.java
+++ src/se/kth/csc/iprog/matchme/android/LevelActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
  
 public class LevelActivity extends Activity {
  
@@ -13,6 +16,16 @@ public class LevelActivity extends Activity {
         setContentView(R.layout.levels_view);
         
         //TODO: Add code for switching screens and starting a view and a controller.
+        
+        Button level1button = (Button) findViewById(R.id.level1_button);
+        level1button.setOnClickListener(new OnClickListener() {
+      			@Override
+      			public void onClick(View arg0) {
+      				//Move to the next view!
+      				Intent i = new Intent(LevelActivity.this, GameActivity.class);
+      				startActivity(i);
+      			}
+      		});   
  
     }
  
