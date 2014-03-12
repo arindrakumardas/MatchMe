09d7f8c64fe0c59e377df75f398dbeba613b52eb
Arindra Kumar Das <arindra.das@live.com>
2014-03-12 15:46:30 +0100

    buttons fixed

--- src/se/kth/csc/iprog/matchme/android/LevelActivity.java
+++ src/se/kth/csc/iprog/matchme/android/LevelActivity.java
@@ -15,17 +15,17 @@ public class LevelActivity extends Activity {
         setContentView(R.layout.levels_view);
         
         //TODO: Add code for switching screens and starting a view and a controller.
-        Button backbtn = (Button) findViewById(R.id.back_btn);
-        backbtn.setOnClickListener(new OnClickListener() {
-      			@Override
-      			public void onClick(View arg0) {
-      				//Move to the next view!
-      				Intent i = new Intent(LevelActivity.this, StartActivity.class);
-      				startActivity(i);
-      			}
-      		}); 
-        
-//        Button level1_button = (Button) findViewById(R.id.level1_button);
+//        Button backbtn = (Button) findViewById(R.id.back_btn);
+//        backbtn.setOnClickListener(new OnClickListener() {
+//      			@Override
+//      			public void onClick(View arg0) {
+//      				//Move to the next view!
+//      				Intent i = new Intent(LevelActivity.this, StartActivity.class);
+//      				startActivity(i);
+//      			}
+//      		}); 
+//        
+//        Button level1_button = (Button) findViewById(R.id.back_btn);
 //        level1_button.setOnClickListener(new OnClickListener() {
 //      			@Override
 //      			public void onClick(View arg0) {
@@ -34,7 +34,25 @@ public class LevelActivity extends Activity {
 //      				startActivity(i);
 //      			}
 //      		});   
-        
+//        
     }
  
+    
+    public void launchClick(View v) {
+        Intent intent = null;
+
+        switch(v.getId()) {
+        case R.id.back_btn:
+            intent = new Intent(LevelActivity.this, StartActivity.class);
+            break;
+
+        case R.id.level1_button:
+            intent = new Intent(LevelActivity.this, GameActivity.class);
+            break;
+
+        // etc, etc
+        }
+        startActivityForResult(intent, 0);
+    };
+    
 }
