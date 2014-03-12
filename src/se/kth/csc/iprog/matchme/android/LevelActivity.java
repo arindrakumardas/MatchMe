702184146e82b87ac9a88dff8e012b0036119aa5
Arindra Kumar Das <arindra.das@live.com>
2014-03-12 17:07:02 +0100

    dynamic levels working

--- src/se/kth/csc/iprog/matchme/android/LevelActivity.java
+++ src/se/kth/csc/iprog/matchme/android/LevelActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.TextView;
  
 public class LevelActivity extends Activity {
  
@@ -33,6 +32,50 @@ public class LevelActivity extends Activity {
       				//Move to the next view!
       				Intent i = new Intent(LevelActivity.this, GameActivity.class);
                     i.putExtra("level_value", "1");		
+      	          startActivity(i);
+      			}
+      		}); 
+        
+        Button level2_button = (Button) findViewById(R.id.level2_button);
+        level2_button.setOnClickListener(new OnClickListener() {
+      			@Override
+      			public void onClick(View arg0) {
+      				//Move to the next view!
+      				Intent i = new Intent(LevelActivity.this, GameActivity.class);
+                    i.putExtra("level_value", "2");		
+      	          startActivity(i);
+      			}
+      		}); 
+        
+        Button level3_button = (Button) findViewById(R.id.level3_button);
+        level3_button.setOnClickListener(new OnClickListener() {
+      			@Override
+      			public void onClick(View arg0) {
+      				//Move to the next view!
+      				Intent i = new Intent(LevelActivity.this, GameActivity.class);
+                    i.putExtra("level_value", "3");		
+      	          startActivity(i);
+      			}
+      		}); 
+        
+        Button level4_button = (Button) findViewById(R.id.level4_button);
+        level4_button.setOnClickListener(new OnClickListener() {
+      			@Override
+      			public void onClick(View arg0) {
+      				//Move to the next view!
+      				Intent i = new Intent(LevelActivity.this, GameActivity.class);
+                    i.putExtra("level_value", "4");		
+      	          startActivity(i);
+      			}
+      		}); 
+        
+        Button level5_button = (Button) findViewById(R.id.level5_button);
+        level5_button.setOnClickListener(new OnClickListener() {
+      			@Override
+      			public void onClick(View arg0) {
+      				//Move to the next view!
+      				Intent i = new Intent(LevelActivity.this, GameActivity.class);
+                    i.putExtra("level_value", "5");		
       	          startActivity(i);
       			}
       		}); 
