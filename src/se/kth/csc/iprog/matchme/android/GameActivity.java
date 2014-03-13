23503e158a89bbdee663c53ebab6baafb58a2378
irene <paspatoufi@gmail.com>
2014-03-13 11:19:47 +0100

    implemented timer

--- src/se/kth/csc/iprog/matchme/android/GameActivity.java
+++ src/se/kth/csc/iprog/matchme/android/GameActivity.java
@@ -3,6 +3,7 @@ package se.kth.csc.iprog.matchme.android;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -24,6 +25,22 @@ public class GameActivity extends Activity {
         String level = i.getStringExtra("level_value");
         txtName.setText(level);
         
+        // implements CountdownTimer
+        
+        new CountDownTimer(30000, 1000) {
+        	TextView timeLeft = (TextView) findViewById(R.id.time_left_value);
+
+            public void onTick(long millisUntilFinished) {
+            	timeLeft.setText("" + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+            	// TODO: set intent to next screen
+            	Intent i = new Intent(GameActivity.this, PauseActivity.class);
+  				startActivity(i);
+            }
+         }.start();
+
         
         Button pausebtn = (Button) findViewById(R.id.pause_btn);
         pausebtn.setOnClickListener(new OnClickListener() {
