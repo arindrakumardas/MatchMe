6dd425afb1d48996971ad12017ab24ff4ad1c06a
Arindra Kumar Das <arindra.das@live.com>
2014-03-19 13:10:20 +0100

    Score working according to level

--- src/se/kth/csc/iprog/matchme/android/EndActivity.java
+++ src/se/kth/csc/iprog/matchme/android/EndActivity.java
@@ -2,6 +2,8 @@ package se.kth.csc.iprog.matchme.android;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -19,14 +21,26 @@ public class EndActivity extends Activity {
 		final String level = intent.getStringExtra("level_value");
 		final int WinTime = intent.getIntExtra("Win_Time", 0);
 		
+		int levelValue = Integer.parseInt(level);
+		
 		
 		//TODO: Score algorithm
 		int baseScoreValue = 100;
 		TextView scorevalue = (TextView) findViewById(R.id.scorevalue);
-		int score = baseScoreValue * WinTime ; 
+		int score = baseScoreValue * WinTime * levelValue ; 
 		String strI = String.valueOf(score);
 		scorevalue.setText(strI);
 
+//
+//		if (score > 0){			
+//			int best_Score = score;
+//			TextView bestScore = (TextView) findViewById(R.id.bestscore);
+//			String best_score = String.valueOf(best_Score);
+//			bestScore.setText(best_score);
+//			
+//		}
+//		
+		
 		// Home Button
 		Button backbtn = (Button) findViewById(R.id.back_btn);
 		backbtn.setOnClickListener(new OnClickListener() {
