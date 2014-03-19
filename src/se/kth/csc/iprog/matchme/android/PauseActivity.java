53a25c25c9ea043d6d6196080839049c4d00cf43
maggud <magnus.gudmandsen@gmail.com>
2014-03-19 14:59:06 +0100

    The home button in EndActivity now works properly.

--- src/se/kth/csc/iprog/matchme/android/PauseActivity.java
+++ src/se/kth/csc/iprog/matchme/android/PauseActivity.java
@@ -8,19 +8,19 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class PauseActivity extends Activity {
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.pause_dialog_view);
-		
+
 		// Receiving the time data
 		Intent intent = getIntent();
 		final String level = intent.getStringExtra("level_value");
-		
+
 		Intent returnIntent = new Intent();
 		setResult(RESULT_OK, returnIntent); //As default.
-		
+
 		// Play (Resume) Button
 		Button playBtn = (Button) findViewById(R.id.play_btn);
 		playBtn.setOnClickListener(new OnClickListener() {
@@ -28,42 +28,42 @@ public class PauseActivity extends Activity {
 			@Override
 			public void onClick(View arg0) {
 				//Simply kill this activity to go back to the game.
-//				Intent i = new Intent(PauseActivity.this, GameActivity.class);
-//				i.putExtra("resumeTime", resumeTime);		// start timer from where stopped
-//				startActivity(i);
+				//				Intent i = new Intent(PauseActivity.this, GameActivity.class);
+				//				i.putExtra("resumeTime", resumeTime);		// start timer from where stopped
+				//				startActivity(i);
 				Intent returnIntent = new Intent();
 				setResult(RESULT_OK, returnIntent);
 				finish(); // finish current activity
 			}
 		});
-		
+
 		// Restart Button
 		Button restartBtn = (Button) findViewById(R.id.restart_btn);
 		restartBtn.setOnClickListener(new OnClickListener() {
-    			@Override
-    			public void onClick(View arg0) { 
-    				Intent returnIntent = new Intent();
-    				setResult(RESULT_CANCELED, returnIntent);//Kill the previous game, and create a new game on the same level.
-    				Intent i = new Intent(PauseActivity.this, GameActivity.class);
-    				i.putExtra("resumeTime", 30000);	// reset timer
-    				i.putExtra("level_value", level);
-    				startActivity(i);
-    				finish(); // finish current activity 
-    			}
-    		});
-		
+			@Override
+			public void onClick(View arg0) { 
+				Intent returnIntent = new Intent();
+				setResult(RESULT_CANCELED, returnIntent);//Kill the previous game, and create a new game on the same level.
+				Intent i = new Intent(PauseActivity.this, GameActivity.class);
+				i.putExtra("resumeTime", 30000);	// reset timer
+				i.putExtra("level_value", level);
+				startActivity(i);
+				finish(); // finish current activity 
+			}
+		});
+
 		// Home Button
 		Button backBtn = (Button) findViewById(R.id.back_btn);
 		backBtn.setOnClickListener(new OnClickListener() {
-    			@Override
-    			public void onClick(View arg0) {
-    				Intent i = new Intent(PauseActivity.this, StartActivity.class);
-    				i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); //Clear the activity stack and start from the StartActivity.
-    				startActivity(i);
-    			}
-    		}); 
+			@Override
+			public void onClick(View arg0) {
+				Intent i = new Intent(PauseActivity.this, StartActivity.class);
+				i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); //Clear the activity stack and start from the StartActivity.
+				startActivity(i);
+			}
+		}); 
 	} 	
-	
+
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
