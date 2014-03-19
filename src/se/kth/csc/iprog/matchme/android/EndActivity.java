865bb00bf2bf643acefe45aee319ff14e74e2977
maggud <magnus.gudmandsen@gmail.com>
2014-03-19 15:17:32 +0100

    Fixed the Home button in EndActivity to work properly

--- src/se/kth/csc/iprog/matchme/android/EndActivity.java
+++ src/se/kth/csc/iprog/matchme/android/EndActivity.java
@@ -18,20 +18,20 @@ public class EndActivity extends Activity {
 		Intent intent = getIntent();
 		final String level = intent.getStringExtra("level_value");
 		final int WinTime = intent.getIntExtra("Win_Time", 0);
-		
+
 		final String lost = (getResources().getString(R.string.end_msg_lose));
 		final String win = (getResources().getString(R.string.end_msg_won));
 
 		int levelValue = Integer.parseInt(level);
-		
-		
+
+
 		//TODO: Score algorithm
 		int baseScoreValue = 100;
 		TextView scorevalue = (TextView) findViewById(R.id.scorevalue);
 		int score = baseScoreValue * WinTime * levelValue ; 
 		String strI = String.valueOf(score);
 		scorevalue.setText(strI);
-		
+
 		if (score==0){
 			TextView game_end_msg = (TextView)findViewById(R.id.game_end_msg);
 			game_end_msg.setText(lost);
@@ -42,29 +42,29 @@ public class EndActivity extends Activity {
 		}
 
 
-//
-//		if (score > 0){			
-//			int best_Score = score;
-//			TextView bestScore = (TextView) findViewById(R.id.bestscore);
-//			String best_score = String.valueOf(best_Score);
-//			bestScore.setText(best_score);
-//			
-//		}
-//		
-		
+		//
+		//		if (score > 0){			
+		//			int best_Score = score;
+		//			TextView bestScore = (TextView) findViewById(R.id.bestscore);
+		//			String best_score = String.valueOf(best_Score);
+		//			bestScore.setText(best_score);
+		//			
+		//		}
+		//		
+
 		// Home Button
-		Button backbtn = (Button) findViewById(R.id.back_btn);
-		backbtn.setOnClickListener(new OnClickListener() {
+		Button backBtn = (Button) findViewById(R.id.back_btn);
+		backBtn.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
-				//Move to the next view!
 				Intent i = new Intent(EndActivity.this, StartActivity.class);
+				i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); //Clear the activity stack and start from the StartActivity.
 				startActivity(i);
 			}
-		});  
-		
+		}); 
 
-		
+
+
 		// Restart Button
 		Button playbtn = (Button) findViewById(R.id.play_btn);
 		playbtn.setOnClickListener(new OnClickListener() {
@@ -79,7 +79,7 @@ public class EndActivity extends Activity {
 			}
 		});
 	}
-	
+
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
